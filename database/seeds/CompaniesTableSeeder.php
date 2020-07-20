7db63aa4aaa3c013d30582c2f4a3e1346c420f31
EricBadalyan <erik_badalyan@yahoo.com>
2020-07-21 00:06:09 +0400

    lesson 2

---
+++ database/seeds/CompaniesTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class CompaniesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        DB::table('companies')->delete();
+        DB::table('companies')->truncate();
+
+        $faker = Faker::create();
+        $companies = [];
+
+        foreach (range(1,10) as $index)
+        {
+            $companies[] = [
+                'name'  =>  $faker->name,
+                'address'   =>  $faker->address,
+                'website'   =>  $faker->domainName,
+                'email'   =>  $faker->email,
+                'created_at'    =>  now(),
+                'updated_at'    =>  now()
+            ];
+        }
+
+        DB::table('companies')->insert($companies);
+    }
+}
